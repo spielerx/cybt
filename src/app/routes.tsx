dc644585efa2d6b76cf61f2f8686f4cdc1f56b0a
Roman <spielerua@gmail.com>
2023-08-04 09:12:14 +0300

    feat: add private route

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -4,7 +4,16 @@ import DashboardLayout from 'layouts/dashboardLayout/DashboardLayout';
 import Error404 from 'pages/error404/Error404';
 import Intro from 'pages/intro/Intro';
 import Login from 'pages/login/Login';
-import { Navigate } from 'react-router-dom';
+import { Navigate, Outlet, useLocation } from 'react-router-dom';
+import { selectIsAuthenticated } from 'slices/user/userSlice';
+import { useAppSelector } from './store';
+
+export default function PrivateRoute() {
+  const isAuthenticated = useAppSelector(selectIsAuthenticated);
+  const location = useLocation();
+
+  return isAuthenticated ? <Outlet /> : <Navigate to="/login" state={{ from: location }} replace />;
+}
 
 export const routes: Route[] = [
   {
@@ -20,18 +29,25 @@ export const routes: Route[] = [
   },
   {
     path: '/',
-    element: <DashboardLayout />,
+    element: <PrivateRoute />,
     children: [
       {
-        path: '',
-        element: <Navigate to="/intro" />,
-      },
-      {
-        path: 'intro',
-        element: <Intro />,
+        path: '/',
+        element: <DashboardLayout />,
+        children: [
+          {
+            path: '',
+            element: <Navigate to="/intro" />,
+          },
+          {
+            path: 'intro',
+            element: <Intro />,
+          },
+        ],
       },
     ],
   },
+
   {
     path: '*',
     element: <Error404 />,
